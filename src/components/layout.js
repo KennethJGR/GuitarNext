f23d9399b01f35c53a029bfe84e24e85dc09cf08
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-03-17 16:06:56 -0600

    adding head to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Head from "next/head";
 
 export default function Layout({ children }) {
     return (
         <>
+            <Head>
+                <title>Guitar Next</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
             <h1>Layout</h1>
 
             {children}
