eec7eaa18a8e4193264dbddfe0f3cbe91e67be48
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-03-18 16:27:28 -0600

    adding title to us

--- src/pages/us.js
+++ src/pages/us.js
@@ -3,7 +3,10 @@ import Layout from "@/components/layout";
 
 const Us = () => {
     return (
-        <Layout>
+        <Layout
+        title="Us"
+        description="This is the us page"
+        >
             us
             <Link href="/"> Home </Link>
         </Layout>
