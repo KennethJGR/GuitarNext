f9cea8d498ae28859979a22861ba785f1d3e7fa5
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-03-20 22:02:56 -0600

    adding css to header

--- src/components/header.js
+++ src/components/header.js
@@ -6,15 +6,11 @@ import styles from "@/styles/header.module.css";
 
 export default function Header() {
     return (
-        <header
-        className={styles.header}
-        >
-            <div className="container">
-                <Image src={logo} alt="GuitarNext" width={200} height={200} />
+        <header className={styles.header}>
+            <div className={`container ${styles.navigation}`}>
+                <Image src={logo} alt="GuitarNext" width={300} height={40} />
 
-                <nav 
-                className={styles.navigation}
-                >
+                <nav >
                     <Link href="/">Home</Link>
                     <Link href="/us">Us</Link>
                     <Link href="/blog">Blog</Link>
