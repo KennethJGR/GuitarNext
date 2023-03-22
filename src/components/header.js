36847bc3f170a8def6b93f54d2ec52d14d4d8b60
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-03-21 22:27:12 -0600

    finishing header css

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
 import logo from "../../public/img/logo.png";
 import styles from "@/styles/header.module.css";
 
 export default function Header() {
+    
+    const router = useRouter();
+
     return (
         <header className={styles.header}>
-            <div className={`container ${styles.navigation}`}>
+            <div className={`container ${styles.bar}`}>
                 <Image src={logo} alt="GuitarNext" width={300} height={40} />
 
-                <nav >
+                <nav className={styles.navigation}>
                     <Link href="/">Home</Link>
                     <Link href="/us">Us</Link>
                     <Link href="/blog">Blog</Link>
