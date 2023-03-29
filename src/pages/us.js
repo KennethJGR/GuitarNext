8fdbcdfc74a61a6479ce927c65a3fb19d1235937
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-03-29 16:04:33 -0600

    creating us page

--- src/pages/us.js
+++ src/pages/us.js
@@ -1,9 +1,34 @@
 import Layout from "@/components/layout";
+import Image from "next/image";
 
 const Us = () => {
     return (
         <Layout title="Us" description="This is the us page">
-            <h1>Us</h1>
+            <main className="container">
+                <h2 className="heading">Us</h2>
+                <div>
+                    <Image
+                        src="/img/us.jpg"
+                        width={1000}
+                        height={800}
+                        alt="Picture in us"
+                    />
+
+                    <div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                            Blanditiis repellat ipsam consequatur, molestias, qui et suscipit
+                            incidunt assumenda architecto aliquam reiciendis repudiandae atque
+                            voluptate voluptatem quia, nostrum voluptates libero numquam.
+                            {""}
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam
+                            quae quod, voluptate, quia, voluptas quibusdam voluptatem quidem,
+                            quos nemo dolorum quas. Quisquam, quod, voluptas, quibusdam, quae,
+                            quidem voluptatem voluptate quia.
+                        </p>
+                    </div>
+                </div>
+            </main>
         </Layout>
     );
 };
