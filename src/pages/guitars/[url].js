19ca95209c92ecb3d9037a729a5fd5203b20550a
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-04-09 12:43:49 -0600

    getstatic url component

--- src/pages/guitars/[url].js
+++ src/pages/guitars/[url].js
@@ -1,25 +1,60 @@
 import React from "react";
 
-export default function Product() {
+export default function Product({ data }) {
+    console.log(data.data[0].attributes.name);
+
+    const { name, price, description, image } = data.data[0].attributes;
+
     return (
         <div>
-            <h1>Product: </h1>
+            <h1>
+                {name} - {price} - {description} -
+            </h1>
         </div>
     );
 }
 
-export async function getServerSideProps({ query: { url } }) {
-    console.log(url);
+export async function getStaticPaths() {
+    const result = await fetch(`${process.env.API_URL}/guitars`);
+    const data = await result.json();
+    console.log(data);
 
+    const paths = data.data.map((guitar) => ({
+        params: { url: guitar.attributes.url },
+    }));
+
+    console.log(paths);
+
+    return {
+        paths,
+        fallback: false,
+    };
+}
+
+export async function getStaticProps({ params: { url } }) {
     const result = await fetch(
         `${process.env.API_URL}/guitars?filters[url]=${url}&populate=image}`
     );
 
     const data = await result.json();
 
-    console.log(data);
-
     return {
-        props: {},
+        props: {
+            data,
+        },
     };
 }
+/* 
+export async function getServerSideProps({ query: { url } }) {
+    const result = await fetch(
+        `${process.env.API_URL}/guitars?filters[url]=${url}&populate=image}`
+    );
+
+    const data = await result.json();
+
+    return {
+        props: {
+            data,
+        },
+    };
+} */
