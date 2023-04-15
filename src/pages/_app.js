1c187d4ec76496e386b53ce10433396e7545c57b
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-04-14 23:06:55 -0600

    creating functions on cart

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,50 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [cart, setCart] = useState([]);
+
+  const addCart = (guitar) => {
+    if (cart.some((guitarState) => guitarState.id === guitar.id)) {
+      const cartUpdate = cart.map((guitarState) => {
+        if (guitarState.id === guitar.id) {
+          guitarState.quantity = guitar.quantity;
+        }
+        return guitarState;
+      });
+
+      setCart([...cartUpdate]);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+      setCart([...cart, guitar]);
+      localStorage.setItem("cart", JSON.stringify(cart));
+    }
+  };
+
+  const deleteProduct = (id) => {
+    const cartUpdate = cart.filter((product) => product.id != id);
+    setCart(cartUpdate);
+    window.localStorage.setItem("cart", JSON.stringify(cart));
+  };
+
+  const updateQuantity = (guitar) => {
+    const cartUpdate = cart.map((guitarState) => {
+      if (guitarState.id === guitar.id) {
+        guitarState.quantity = parseInt(guitar.quantity);
+      }
+      return guitarState;
+    });
+    setCart(cartUpdate);
+    window.localStorage.setItem("cart", JSON.stringify(cart));
+  };
+
+  return (
+    <Component
+      {...pageProps}
+      cart={cart}
+      addCart={addCart}
+      deleteProduct={deleteProduct}
+      updateQuantity={updateQuantity}
+    />
+  );
 }
--- src/pages/guitars/[url].js
+++ src/pages/guitars/[url].js
@@ -1,16 +1,35 @@
 import React from "react";
+import { useState } from "react";
 import Image from "next/image";
 import styles from "../../styles/guitars.module.css";
 import Layout from "@/components/layout";
 
-export default function Product({ guitar }) {
+export default function Product({ guitar, addCart }) {
+    const [quantity, setQuantity] = useState(0);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (quantity < 1) {
+            alert("Please select a quantity");
+            return;
+        }
+
+        const selectGuitar = {
+            id: guitar[0].id,
+            name: guitar[0].attributes.name,
+            price: guitar[0].attributes.price,
+            image: guitar[0].attributes.image.data.attributes.url,
+            quantity,
+        };
+
+        addCart(selectGuitar);
+    };
+
     const { name, price, description, image } = guitar[0].attributes;
     console.log(guitar[0].attributes);
 
     return (
-        <Layout
-            title={`Guitar ${name}`}
-        >
+        <Layout title={`Guitar ${name}`}>
             <div className={styles.guitar}>
                 <Image
                     src={image.data.attributes.url}
@@ -22,6 +41,22 @@ export default function Product({ guitar }) {
                     <h2>{name}</h2>
                     <p className={styles.description}>{description}</p>
                     <p className={styles.price}>${price}</p>
+
+                    <form onSubmit={handleSubmit} className={styles.form}>
+                        <label htmlFor="quantity">Quantity:</label>
+                        <select
+                            onChange={(e) => setQuantity(+e.target.value)}
+                            id="quantity"
+                        >
+                            <option value="0">Select</option>
+                            <option value="1">1</option>
+                            <option value="2">2</option>
+                            <option value="3">3</option>
+                            <option value="4">4</option>
+                            <option value="5">5</option>
+                        </select>
+                        <input type="submit" value={`Add to cart`} />
+                    </form>
                 </div>
             </div>
         </Layout>
