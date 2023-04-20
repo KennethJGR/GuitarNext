626dd9a079722df3ee425795a5a0c3b60cdf65d2
Kenneth Gaitan <gaitan.kj@gmail.com>
2023-04-19 21:32:00 -0600

    creating cart css

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,8 +1,9 @@
 import Layout from "@/components/layout";
 import React from "react";
 import styles from "@/styles/cart.module.css";
+import Image from "next/image";
 
-export default function Cart() {
+export default function Cart({ cart }) {
     return (
         <Layout title="Cart">
             <main className="container">
@@ -10,6 +11,33 @@ export default function Cart() {
                 <div className={styles.content}>
                     <div className={styles.cart}>
                         <h2>Product Name</h2>
+
+                        {cart.length === 0
+                            ? "Your cart is empty"
+                            : cart.map((item) => (
+                                <div key={item.id} className={styles.product}>
+                                    <div>
+                                        <Image
+                                            src={item.image}
+                                            alt={item.name}
+                                            width={100}
+                                            height={100}
+                                        />
+                                    </div>
+                                    <div>
+                                        <p className={styles.name}>
+                                            Product Name: <span>{item.name}</span>
+                                        </p>
+                                        <p>Quantity: {item.quantity}</p>
+                                        <p className={styles.price}>
+                                            Price: <span>${item.price}</span>
+                                        </p>
+                                        <p className={styles.subtotal}>
+                                            Subtotal: <span>${item.quantity * item.price}</span>
+                                        </p>
+                                    </div>
+                                </div>
+                            ))}
                     </div>
 
                     <aside className={styles.summary}>
